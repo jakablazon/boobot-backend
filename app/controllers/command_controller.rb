b494a35ba7ce3a04eafe808bd4d54e9c51221a39
Blaz Cigale <blaz.cigale@gmail.com>
2017-01-17 23:11:50 +0100

    Dynamic image search

--- app/controllers/command_controller.rb
+++ app/controllers/command_controller.rb
@@ -1,47 +1,89 @@
 class CommandController < ApplicationController
 
-  skip_before_filter :verify_authenticity_token, :only => :boobot
+  skip_before_action :verify_authenticity_token, :only => :boobot
+
+  require 'open-uri'
+  require 'nokogiri'
+  require 'cgi'
 
   def boobot
 
-    nakedGirls = [
-        'http://c1.nuttit.com/Cache/GetImage?id=FoGJk4A.jpg',
-        'http://c3.nuttit.com/Cache/GetImage?id=6VVXqgN.png',
-        'http://c1.nuttit.com/Cache/GetImage?id=TmBCHmn.jpg',
-        'http://c1.nuttit.com/Cache/GetImage?id=VxNH8Bi.jpg',
-        'http://c1.nuttit.com/Cache/GetImage?id=gKjHmbT.jpg',
-        'http://c2.nuttit.com/Cache/GetImage?id=MFPTO3Y.jpg',
-        'http://c3.nuttit.com/Cache/GetImage?id=Jre99Xa.jpg',
-        'https://68.media.tumblr.com/b6663bd10a3acae8b47e9a0072340689/tumblr_nfp5mqeQoE1u4z5uqo1_500.gif',
-        'http://c2.nuttit.com/Cache/GetImage?id=Lj5tr6c.jpg',
-        'http://c3.nuttit.com/Cache/GetImage?id=vso6IlX.png',
-        'http://c1.nuttit.com/Cache/GetImage?id=5t4j2mU.png',
-        'http://c3.nuttit.com/Cache/GetImage?id=hbrIgcx.gif',
-        'http://c3.nuttit.com/Cache/GetImage?id=zfOecQe.png',
-        'http://c3.nuttit.com/Cache/GetImage?id=4PQIfmj.jpg',
-        'http://c1.nuttit.com/Cache/GetImage?id=6VVXqgN.png',
-        'http://c2.nuttit.com/Cache/GetImage?id=6yjvYXN.jpg',
-        'http://c2.nuttit.com/Cache/GetImage?id=Xbyv4dv.png',
-        'http://c3.nuttit.com/Cache/GetImage?id=ArtNpNS.jpg',
-        'http://c1.nuttit.com/Cache/GetImage?id=sb5Zhr2.jpg',
-        'http://c2.nuttit.com/Cache/GetImage?id=uDwbP8s.jpg',
-        'http://c3.nuttit.com/Cache/GetImage?id=4JOPCHS.jpg',
-        'http://c2.nuttit.com/Cache/GetImage?id=oEyh0xj.jpg'
+    listOfRandomSearches = [
+        'ass',
+        'tits',
+        'doggy',
+        'naked',
+        'cum',
+        'blowjob',
+        'sex',
+        'creampie',
+        'anal',
+        'pussy',
+        'eyes',
+        'glasses',
+        'slut',
+        'bikini',
+        'cutie',
+        'finger',
+        'milf',
+        'buttsex',
+        'hard',
+        'spread',
+        'threesome',
+        'vibrator',
+        'buttplug',
+        'spanking'''
     ]
 
-    randomImage = nakedGirls.sample
+    if params[:text].present?
+      query = params[:text]
+      text = "You searched for `" + query + "`"
+    else
+      query = listOfRandomSearches.sample
+      text = "We searched `" + query + "` for you "
+    end
 
+    # Encode Query String
+    query = query.gsub!(/[^a-z ]/, '')
+
+    logger.info query
+
+    # Get Number Of Pages By Search Query
+    doc = Nokogiri::HTML(open("http://anyfap.com/" + query))
+    pages = doc.css('.page-btn')
+
+    # Get Random Page
+    randomPage = rand(1..pages.length)
+
+    # Open On That Random Page
+    doc = Nokogiri::HTML(open("http://anyfap.com/" + query + "?images=true&videos=false&order=relevance&page=" + randomPage.to_s))
+
+    # Get Number Of Results
+    images = doc.css('.row .col-md-2')
+
+    # Get Random Image
+    randomImage = rand(1..images.length)
+
+    # Get Random Image URL And Title
+    mainImage = images.css('.thumb')[randomImage]['src']
+    mainImageTitle = images.css('.title-grid p')[randomImage].text
+
+    if mainImageTitle.empty? || mainImage.empty?
+      abort("Shit hit the roof.")
+    end
+
+    # Build Command Response
     data = {
         "response_type" => "in_channel",
         "attachments" => [
-            "color"=> "#36a64f",
-            "pretext"=> "Desired image",
-            "title"=> "Test",
-            "title_link"=> "https://api.slack.com/",
-            "text"=> "You searched for:",
-            "image_url"=> randomImage,
-            "thumb_url"=> randomImage,
-            "ts"=> DateTime.now.strftime('%s'),
+            "color"       => "#36a64f",
+            "pretext"     => "Here, relax your eyes for a while.",
+            "title"       => mainImageTitle,
+            "title_link"  => mainImage,
+            "text"        => text,
+            "image_url"   => mainImage,
+            "thumb_url"   => mainImage,
+            "ts"          => DateTime.now.strftime('%s'),
         ]
     }
 
