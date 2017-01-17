31604dbcbc91721fe07e5d894d9639a71a0d9579
Blaz Cigale <blaz.cigale@gmail.com>
2017-01-17 19:21:38 +0100

    Random hardcoded image

--- app/controllers/command_controller.rb
+++ app/controllers/command_controller.rb
@@ -4,15 +4,32 @@ class CommandController < ApplicationController
 
   def boobot
 
+    nakedGirls = [
+        'http://c1.nuttit.com/Cache/GetImage?id=FoGJk4A.jpg',
+        'http://c3.nuttit.com/Cache/GetImage?id=6VVXqgN.png',
+        'http://c1.nuttit.com/Cache/GetImage?id=TmBCHmn.jpg',
+        'http://c1.nuttit.com/Cache/GetImage?id=VxNH8Bi.jpg',
+        'http://c1.nuttit.com/Cache/GetImage?id=gKjHmbT.jpg',
+        'http://c2.nuttit.com/Cache/GetImage?id=MFPTO3Y.jpg',
+        'http://c3.nuttit.com/Cache/GetImage?id=Jre99Xa.jpg',
+        'https://68.media.tumblr.com/b6663bd10a3acae8b47e9a0072340689/tumblr_nfp5mqeQoE1u4z5uqo1_500.gif',
+        'http://c2.nuttit.com/Cache/GetImage?id=Lj5tr6c.jpg',
+        'http://c3.nuttit.com/Cache/GetImage?id=vso6IlX.png',
+        'http://c1.nuttit.com/Cache/GetImage?id=5t4j2mU.png',
+        'http://c3.nuttit.com/Cache/GetImage?id=hbrIgcx.gif'
+    ]
+
+    randomImage = nakedGirls.sample
+
     data = {
         'attachments' => [
             "color"=> "#36a64f",
             "pretext"=> "Desired image",
             "title"=> "Test",
             "title_link"=> "https://api.slack.com/",
-            "text"=> "You searched for: keyword",
-            "image_url"=> "http://placehold.it/350x150",
-            "thumb_url"=> "hhttp://placehold.it/100x100",
+            "text"=> "You searched for:",
+            "image_url"=> randomImage,
+            "thumb_url"=> randomImage,
             "ts"=> DateTime.now.strftime('%s'),
         ]
     }
